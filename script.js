3d42a4e1410a7f826d0f47ff2e1e66b36668574e
Seb <notfork.sigma@gmail.com>
2025-04-19 18:42:41 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const galleryGrid = document.querySelector('.gallery-grid');
+
+function loadGallery() {
+  const images = ['art1.jpg', 'art2.jpg', 'art3.jpg'];
+  images.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = `images/${image}`;
+    imgElement.alt = "Artwork";
+    galleryGrid.appendChild(imgElement);
+  });
+}
+
+// Call loadGallery on page load
+window.onload = loadGallery;
